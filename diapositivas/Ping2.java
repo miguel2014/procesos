ecee859cb32035b7584e9c280eeba07533d06e6c
minombre <miccorreo@gmail.com>
2015-09-29 13:04:56 +0200

    new file:   diapositivas/Ping2.java

---
+++ diapositivas/Ping2.java
@@ -0,0 +1,60 @@
+package diapositivas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ping2 {
+	public static void main(String[] args) {
+		//Pasamos como argumentos el host al que vamos a hacer ping
+		BufferedReader in=null;
+		List <String> lista=new ArrayList<String>();
+		List <String> comandos=new ArrayList<String>();
+		ProcessBuilder pBuilder=null;
+		String[] campos=null;
+		if (args.length>0) {
+			comandos.add("ping");
+			comandos.add(args[0]);
+			comandos.add("-c4");
+			pBuilder=new ProcessBuilder(comandos);
+		}
+		else{
+			System.out.println("Has de introducir argumentos");
+			System.exit(0);
+		}
+		try {
+			Process proceso=pBuilder.start();
+			in=new BufferedReader
+					(new InputStreamReader(proceso.getInputStream()));
+			in.readLine();
+			String linea=null;
+			while ((linea=in.readLine())!=null) {
+				if (lista.size()<4) {
+					lista.add(linea);	
+				}	
+			}
+			/*for (int i = 0; i < lista.size(); i++) {
+				campos=lista.get(i).split("/t");
+				
+			}*/
+			System.out.println(lista);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if (in!=null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
