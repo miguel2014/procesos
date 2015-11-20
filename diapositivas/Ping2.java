f9c2bd56d93c563d249a478aa05733a80b180ae7
minombre <miccorreo@gmail.com>
2015-11-20 14:15:10 +0100

    new file:   diapositivas/JFrame.java 	modificado: diapositivas/Ping2.java 	modificado: diapositivas/Ventana.java

--- diapositivas/Ping2.java
+++ diapositivas/Ping2.java
@@ -15,16 +15,19 @@ public class Ping2 {
 		List <String> listadatos2=new ArrayList<String>(); //Para datos ,min,max y media
 		int tamanoInicial;
 		int tamanoFinal;
+		
 		String linea;
 		List <String> comandos=new ArrayList<String>();
 		ProcessBuilder pBuilder=null;
+		final String paquetes="4";
 		String[] campos=null;
-		if (args.length>0) {
+		
 			comandos.add("ping");
 			comandos.add("www.google.es");
-			comandos.add("-c4");
+			comandos.add("-c");
+			comandos.add(paquetes);
 			pBuilder=new ProcessBuilder(comandos);
-		}
+		
 		try {
 			Process proceso=pBuilder.start();
 			in=new BufferedReader	(new InputStreamReader(proceso.getInputStream()));
@@ -38,30 +41,9 @@ public class Ping2 {
 				System.out.println("Se introdujo mal la URL ");
 				System.exit(0);
 			}
-			campos=listadatos1.get(6).split("\\s");
-			tamanoInicial=Integer.parseInt(campos[0]);
-			tamanoFinal=Integer.parseInt(campos[3]);
 			
-			if (tamanoInicial !=tamanoFinal) {
-				System.out.println("Se produjo un fallo en la conexión");
-				System.exit(0);
-			}
-			//Bucle que meten datos a lista2
 			
-			for (int i = 0; i < 4; i++) {
-				campos=listadatos1.get(i).split("\\s");
-				linea=campos[7].substring(5, campos[7].length());
-				listadatos2.add(linea);
-				}
-			Collections.sort(listadatos2);
-			System.out.println(listadatos2);
-			System.out.println("Min: "+listadatos2.get(0)+" ms.");
-			System.out.println("Max: "+listadatos2.get((listadatos2.size()-1))+" ms.");
-			double suma=0;
-			for (int i = 0; i < listadatos2.size(); i++) {
-				suma+=Double.parseDouble(listadatos2.get(i));
-			}
-			System.out.println("Media: "+Math.rint((suma/listadatos2.size())*100)/100 +" ms.");
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- diapositivas/Ventana.java
+++ diapositivas/Ventana.java
@@ -13,54 +13,43 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
 public class Ventana extends JFrame{
-    JPanel panel;
-    private JTextField textField;
-    public Ventana(){
-        setTitle("Grafica de ping");
-        setSize(800,600);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        init();
-    }
+	 JPanel panel;
+	    public Ventana(){
+	        setTitle("Como Hacer Graficos con Java");
+	        setSize(800,600);
+	        setLocationRelativeTo(null);
+	        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	        setVisible(true);
+	        init();
+	    }
+	 
+	    private void init() {
+	        panel = new JPanel();
+	        getContentPane().add(panel);
+	        // Fuente de Datos
+	        
+	        
+	        DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
  
-    private void init() {
-       // panel = new JPanel();
-        GridBagConstraints layout;
-        //getContentPane().add(panel);
-        // Fuente de Datos
-        DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
-        line_chart_dataset.addValue(80, "ping", "1");
-        line_chart_dataset.addValue(300, "ping", "2");
-        line_chart_dataset.addValue(60.50, "ping", "3");
-        line_chart_dataset.addValue(1200, "ping", "4");
-        line_chart_dataset.addValue(2400, "ping", "5");  
- 
-        // Creando el Grafico
-        JFreeChart chart=ChartFactory.createLineChart("Grafico de ping",
-                "Paquetes","Tiempo",line_chart_dataset,PlotOrientation.VERTICAL,true,true,false);  
-        
-        // Mostrar Grafico
-        ChartPanel chartPanel = new ChartPanel(chart);
-        panel.add(chartPanel);
-        GridBagLayout gbl_chartPanel = new GridBagLayout();
-        gbl_chartPanel.columnWidths = new int[]{283, 114, 0};
-        gbl_chartPanel.rowHeights = new int[]{19, 0};
-        gbl_chartPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-        gbl_chartPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-        chartPanel.setLayout(gbl_chartPanel);
-        
-        textField = new JTextField();
-        layout = new GridBagConstraints();
-        layout.anchor = GridBagConstraints.NORTHWEST;
-        layout.gridx = 1;
-        layout.gridy = 0;
-        chartPanel.add(textField, layout);
-        textField.setColumns(10);
-    }
-    
-    public static void main(String args[]){
-        new Ventana().setVisible(true);
-    }
-}
+	        for (int i = 0; i < 10; i++) {
+	        	line_chart_dataset.addValue(10, "ping","a" );
+			}
+	        
+	        line_chart_dataset.addValue(600, "ping", "2");
+	        line_chart_dataset.addValue(1200, "ping", "3");
+	        line_chart_dataset.addValue(2400, "ping", "4");
+	        // Creando el Grafico
+	        JFreeChart chart=ChartFactory.createLineChart("Trafico en el Blog",
+	                "Mes","Visitas",line_chart_dataset,PlotOrientation.VERTICAL,
+	                true,true,false);  
+	        
+	        // Mostrar Grafico
+	        ChartPanel chartPanel = new ChartPanel(chart);
+	        panel.add(chartPanel);
+	    }
+	    
+	    public static void main(String args[]){
+	        new Ventana().setVisible(true);
+	    }
+	}
 
